b4a47c786e7f4105a3fac9d09016c3c6c4fa8b7d
Saad Noor <saadnoor21@gmail.com>
2024-07-05 21:55:24 -0700

    Create caeser_cipher.py

---
+++ Caesar Cipher/caeser_cipher.py
@@ -0,0 +1,19 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+def caesar(direction, text, shift):
+  end_text = ""
+  if direction == "decode":
+    shift *= -1
+  for letter in text:
+    position = alphabet.index(letter)
+    new_position = position + shift
+    end_text += alphabet[new_position]
+  print(f"The {direction}d text is {end_text}")
+
+caesar(direction, text, shift)
+
