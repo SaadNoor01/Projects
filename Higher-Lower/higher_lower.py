6ad04a691cfeb5ed324ab6988186be2c73130283
Saad Noor <saadnoor21@gmail.com>
2024-07-11 14:14:17 -0400

    Update higher_lower.py

--- Higher-Lower/higher_lower.py
+++ Higher-Lower/higher_lower.py
@@ -1,68 +1,62 @@
 import random
-from replit import clear
-from art import logo
-from art import vs
+from art import logo, vs
 from game_data import data
+from replit import clear
 
 
-def introduction():
-  print(logo)
+def format_info(option_values):
+  """Takes a list and formats the information and 
+  returns a string with relevlant info for the user"""
+  name = option_values[0]
+  description = option_values[2]
+  country = option_values[3]
+  return f"{name}, {description}, from {country}"
 
-intro = introduction()
 
-questions_chosen = []
-score = 0
-first_option = random.choice(data)
-second_option = random.choice(data)
-
-# make sure they are not the same
-def loop_game(first_option, second_option):
-  
-  while first_option == second_option:
-    second_option = random.choice(data)
-  
-  second_option_ind = data.index(second_option)
-  questions_chosen.append(second_option_ind)
-  
-  # get_values function
-  first_option_values = list(first_option.values())
-  second_option_values = list(second_option.values())
-  
-  # print statement
-  print(f"Compare A: {first_option_values[0]}, {first_option_values[2]}, from {first_option_values[3]}")
-  print(vs)
-  print(f"Compare B: {second_option_values[0]}, {second_option_values[2]}, from {second_option_values[3]}")
-  
-  followers = [first_option_values[1], second_option_values[1]]
-  
-  correct_choice = 0
-  for x in followers:
-    if x > correct_choice:
-      correct_choice = x
-
-  correct_answer = ""
-  ind_ans = followers.index(correct_choice)
-  if ind_ans == 0:
-    correct_answer = "a"
-  elif ind_ans == 1:
-    correct_answer = "b"
-
-  user_choice = input("Who has more followers: Type 'A' or 'B': ").lower()
-  
-  if user_choice == correct_answer:
-    score = len(questions_chosen)
-    clear()
-    introduction()
-    print(f"You're right! Current Score: {score}.")
-    first_option_new = second_option
-    second_option_new = random.choice(data)
-    loop_game(first_option_new, second_option_new)
-
+def check_answer(first_option_followers, second_option_followers, user_choice):
+  """Checks whether the users selection is correct and returns 
+    True or False value"""
+  if first_option_followers > second_option_followers:
+    return user_choice == "a"
   else:
-    score = len(questions_chosen) - 1
+    return user_choice == "b"
+
+
+def play_game():
+  print(logo)
+  score = 0
+  second_option = random.choice(data)
+  play = True
+  
+  while play:
+    first_option = second_option
+    second_option = random.choice(data)
+    
+    while first_option == second_option:
+      second_option = random.choice(data)
+    
+    first_option_values = list(first_option.values())
+    second_option_values = list(second_option.values())
+    
+    print(f"Compare A: {format_info(first_option_values)}")
+    print(vs)
+    print(f"Compare B: {format_info(second_option_values)}")
+    
+    first_option_followers = first_option["follower_count"]
+    second_option_followers = second_option["follower_count"]
+    
+    user_choice = input("Who has more followers: Type 'A' or 'B': ").lower()
+    correct = check_answer(first_option_followers, second_option_followers, user_choice)
+    
     clear()
-    introduction()
-    print(f"Sorry, that's the wrong answer! Final Score: {score}")
-  
-  
-loop_game(first_option, second_option)
+    print(logo)
+
+    if correct:
+      score += 1
+      print(f"You're right! Current Score: {score}.")
+    else:
+      print(f"Sorry, that's the wrong answer! Final Score: {score}")
+      play = False
+
+if __name__ == "__main__":
+  play_game()
