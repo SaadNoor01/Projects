2b50a22c3ee5d0a4aa69092a5beaa28dad8fe329
Saad Noor <saadnoor21@gmail.com>
2024-08-06 15:29:14 -0700

    Update scoreboard.py

--- Snake Game/scoreboard.py
+++ Snake Game/scoreboard.py
@@ -11,15 +11,30 @@ class Scoreboard(Turtle):
         self.board.hideturtle()
         self.board.goto(0, 270)
         self.score = 0
-        self.board.write(f"Score: {self.score}", False, "center", ("Courier", 24, "normal"))
+        with open("data.txt", mode="r") as file:
+            self.highscore = int(file.read())
 
+        # self.highscore = int(content)
+        self.board.write(f"Score: {self.score}   High Score: {self.highscore}", False, "center",
+                         ("Courier", 24, "normal"))
+
+    def reset_sc(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(f"{self.highscore}")
+        self.score = 0
+        self.update_scoreboard()
 
     def gameover(self):
         self.board.goto(0, 0)
         self.board.write("GAME OVER", False, "center", ("Courier", 24, "normal"))
 
-
     def update_scoreboard(self):
         self.board.clear()
+        self.board.write(f"Score: {self.score}   High Score: {self.highscore}", False, "center",
+                         ("Courier", 24, "normal"))
+
+    def increase_score(self):
         self.score += 1
-        self.board.write(f"Score: {self.score}", False, "center", ("Courier", 24, "normal"))
+        self.update_scoreboard()
